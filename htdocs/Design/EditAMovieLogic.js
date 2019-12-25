1c95e6b663664acf26b7b726c97f0c5eb9422d52
Zornitsa Yohanova <45259912+zornitsayohanova@users.noreply.github.com>
2019-12-25 19:39:51 +0200

    Add files via upload

---
+++ htdocs/Design/EditAMovieLogic.js
@@ -0,0 +1,29 @@
+function multipleFunc() {
+    document.getElementById("mySelect").multiple = true;
+}
+
+
+function validateFields() {
+    //validation for Category
+    /**/
+
+    if (document.getElementById('title-field').value === "" ||
+        document.getElementById('poster-field').value === "" ||
+        document.getElementById('director-field').value === "" ||
+        document.getElementById('awards-field').value === "" ||
+        document.getElementById('music-field').value === "" ||
+        document.getElementById('studio-field').value === "" ||
+        document.getElementById('language-field').value === "" ||
+        document.getElementById('release-field').value === "" ||
+        document.getElementById('trailer-field').value === "" ||
+        document.getElementById('duration-field').value === "" ||
+        document.getElementById('country-field').value === "" ||
+        document.getElementById('location-field').value === "" ||
+        document.getElementById('rating-field').value === "" ||
+        document.getElementById('stars-field').value === "" ||
+        document.getElementById('description-area').value === "" ) {
+        alert("Please fill all the fields!");
+    }else {
+        alert("cool");
+    }
+}
